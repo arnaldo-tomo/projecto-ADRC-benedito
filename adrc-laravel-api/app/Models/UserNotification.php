05270f0f3408caa959ff0ed6e5948fe43b3403ce
Arnaldo tomo <arnaldotomo@gmail.com>
2025-07-16 22:40:40 +0200

    Update UserNotification.php

--- adrc-laravel-api/app/Models/UserNotification.php
+++ adrc-laravel-api/app/Models/UserNotification.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserNotification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'notification_id',
+        'is_read',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'is_read' => 'boolean',
+        'read_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function notification()
+    {
+        return $this->belongsTo(Notification::class);
+    }
 }
