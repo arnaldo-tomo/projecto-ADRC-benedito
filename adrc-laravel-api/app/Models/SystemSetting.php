bf3e3f4c24e37bf75d5b8b72c793474bc119b9f4
Arnaldo tomo <arnaldotomo@gmail.com>
2025-07-16 22:40:36 +0200

    Update SystemSetting.php

--- adrc-laravel-api/app/Models/SystemSetting.php
+++ adrc-laravel-api/app/Models/SystemSetting.php
@@ -2,9 +2,52 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SystemSetting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'description',
+    ];
+
+    public static function get($key, $default = null)
+    {
+        $setting = self::where('key', $key)->first();
+        if (!$setting) {
+            return $default;
+        }
+
+        return match($setting->type) {
+            'boolean' => filter_var($setting->value, FILTER_VALIDATE_BOOLEAN),
+            'integer' => (int) $setting->value,
+            'float' => (float) $setting->value,
+            'array' => json_decode($setting->value, true),
+            'json' => json_decode($setting->value),
+            default => $setting->value
+        };
+    }
+
+    public static function set($key, $value, $type = 'string', $description = null)
+    {
+        $formattedValue = match($type) {
+            'boolean' => $value ? '1' : '0',
+            'array', 'json' => json_encode($value),
+            default => (string) $value
+        };
+
+        return self::updateOrCreate(
+            ['key' => $key],
+            [
+                'value' => $formattedValue,
+                'type' => $type,
+                'description' => $description
+            ]
+        );
+    }
 }
